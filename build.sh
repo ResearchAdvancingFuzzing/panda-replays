bc44a973275c72b4215193b812d236b47c090f8a
Tim Leek <trleek@gmail.com>
2020-02-12 18:25:58 -0800

    grab dockerfile from panda

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-cd panda
+rm Dockerfile_18_04*
+
+#wget https://raw.githubusercontent.com/panda-re/panda/spitfire/panda/docker/Dockerfile_18_04
+wget https://raw.githubusercontent.com/panda-re/panda/master/panda/docker/Dockerfile_18_04
+
+#cd panda
+
 docker build -f ./Dockerfile_18_04 -t panda .
 
 cd ..
