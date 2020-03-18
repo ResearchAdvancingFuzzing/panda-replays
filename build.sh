1b6a618dcd99fdf3038b272b6ef50d57d9c7a84b
Andy <andy@bigandian.com>
2020-03-18 17:42:29 -0400

    Fixed tempfile cleanup and now install pyyaml for real

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-rm Dockerfile_18_04*
+rm Dockerfile_18_04* || true
 
 #wget https://raw.githubusercontent.com/panda-re/panda/spitfire/panda/docker/Dockerfile_18_04
 wget https://raw.githubusercontent.com/panda-re/panda/master/panda/docker/Dockerfile_18_04
-sed '/^WORKDIR \"\/panda\/panda\/pypanda\"/i RUN pip install pyyaml'
+sed '/^WORKDIR \"\/panda\/panda\/pypanda\"/i RUN pip install pyyaml' Dockerfile_18_04 > Dockerfile_18_04.patched
 #cd panda
 
-docker build -f ./Dockerfile_18_04 -t panda .
+docker build -f ./Dockerfile_18_04.patched -t panda .
 
 mkdir qcows
