be4d27e5c7f8670f7b93f8d80f70bdd04d631e3f
Tim Leek <trleek@gmail.com>
2020-10-16 16:14:54 -0400

    adding a version of xmllint with a bug manually injected

---
+++ targets/xmllint-3e7e75bed2cf2853b0d42d635d36676b3330d475-64bit-bug/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+docker build --no-cache -t panda-xmllint .
+rm -rf install
+mkdir install
+docker run -v `pwd`/install:/install panda-xmllint cp -r libxml2 install
+# make installed version NOT root:root
+# but owned by current user and has whatever group that person has
+sudo chown -R $USER:"`id -g -n`" install
+
+
+QCOW_URL=`grep qcow yamlfile | awk '{print $2}'`
+#echo QCOW_URL $QCOW_URL
+
+QCOW_BASE=`basename $QCOW_URL`
+#echo QCOW_BASE $QCOW_BASE
+
+QCOW_FILE="../../qcows/$QCOW_BASE"
+
+if test -f "$QCOW_FILE"; then
+    echo "Qcow present: $QCOW_BASE"
+else
+    echo "Obtaining qcow: $QCOW_BASE"
+    wget -O $QCOW_FILE $QCOW_URL
+fi
+
+   
---
+++ targets/xmllint-3e7e75bed2cf2853b0d42d635d36676b3330d475-64bit-bug/run.py
@@ -0,0 +1,104 @@
+
+# run.py yamlfile
+
+import sys
+import os
+from stat import *
+import glob
+from os.path import basename
+import subprocess as sp
+import yaml
+
+from pandare import Panda, blocking
+
+y = yaml.load(open(sys.argv[1]), Loader=yaml.FullLoader)
+
+
+inputfile = sys.argv[2]
+
+
+assert "installdir" in y
+assert "qcow" in y
+assert "snapshot" in y
+assert "copydir" in y
+
+# naming?
+qcowfile = basename(y["qcow"])
+
+
+# input file should exist
+assert(os.path.exists(inputfile))
+assert(os.path.isfile(inputfile))
+
+print("installdir: [%s]" % y["installdir"])
+
+# install dir should exist
+# input file should exist
+assert(os.path.exists(y["installdir"]))
+assert(os.path.isdir(y["installdir"]))
+
+# qcowfile should exist
+qcf = "../../qcows/" + qcowfile
+if not os.path.exists(qcf):
+    if not os.path.exists("../qcows"):
+        os.makedirs("../qcows")
+    sp.check_call(["wget", y["qcow"], "-O", qcf])
+
+
+assert(os.path.isfile(qcf))
+
+
+import shutil
+
+if os.path.exists(y["copydir"]):
+    shutil.rmtree(y["copydir"])
+os.makedirs(y["copydir"])
+
+# copy inputfile and installdir
+shutil.copy(inputfile, y["copydir"])
+shutil.copytree(y["installdir"], y["copydir"]+"/install")
+
+# we'll write replay files here
+# but bc of -v magic that will be the right host location
+replayname = "/replay/" + basename(inputfile) + "-panda"
+print ("replay name = [%s]" % replayname)
+
+@blocking
+def record_cmds():
+
+    panda.revert_sync(y["snapshot"])
+    panda.copy_to_guest(y["copydir"], iso_name="foo.iso")
+
+    cmd = "cd copydir/install/libxml2/.libs && ./xmllint ~/copydir/"+basename(inputfile)
+    panda.type_serial_cmd(cmd)
+
+    print(f"Beginning recording: {replayname}")
+    panda.run_monitor_cmd("begin_record {}".format(replayname)) # Begin recording
+
+    result = panda.finish_serial_cmd() 
+
+    panda.run_monitor_cmd("end_record")
+
+    print(f"Ran command `{cmd}`")
+    print(f"Result: {result}")
+
+    panda.end_analysis()
+
+
+    
+ 
+
+panda = Panda(arch="x86_64", expect_prompt=rb"root@ubuntu:.*#", qcow=qcf, mem="1G", extra_args="-display none -nographic")
+panda.queue_async(record_cmds)
+panda.run()
+
+
+
+with open ("/replay/run.log", "w") as ml:
+    for fn in glob.glob("%s-rr-*" % replayname):
+        ml.write ("chmod %s\n" % fn)
+        os.chmod(fn, S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IROTH)
+
+
+
+
