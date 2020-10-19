9c65e8d29c7aceb78370a4bc07ec115bd54118e8
Tim Leek <trleek@gmail.com>
2020-10-19 09:41:45 -0400

    fixup permissions after creating recording

--- targets/xmllint-3e7e75bed2cf2853b0d42d635d36676b3330d475-64bit/run.sh
+++ targets/xmllint-3e7e75bed2cf2853b0d42d635d36676b3330d475-64bit/run.sh
@@ -31,3 +31,4 @@ RD=$HERE
 echo docker run -v $PRD:/panda-replays -v $ID:/input -v $RD:/replay panda bash -c "cd /panda-replays/targets/xmllint-3e7e75bed2cf2853b0d42d635d36676b3330d475-64bit; python3.6 ./run.py ./yamlfile /input/$IB"
 docker run -v $PRD:/panda-replays -v $ID:/input -v $RD:/replay panda bash -c "cd /panda-replays/targets/xmllint-3e7e75bed2cf2853b0d42d635d36676b3330d475-64bit; python3.6 ./run.py ./yamlfile /input/$IB"
 
+sudo chown -R $USER.$USER run.log foo.iso copydir $IB-panda-rr-nondet.log $IB-panda-rr-snp 
